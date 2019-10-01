09ebeeeda88944ced1a97919cfbed7afc1749942
astrobeard <astrobeard21@gmail.com>
2019-10-01 15:07:31 -0400

    halos repository created

---
+++ halos.h
@@ -0,0 +1,20 @@
+
+#ifndef HALOS_H 
+#define HALOS_H 
+
+#ifdef __cplusplus 
+extern "C" { 
+#endif /* __cplusplus */ 
+
+#include "src/objects.h" 
+#include "src/catalogue.h" 
+#include "src/halo.h" 
+#include "src/io/columns.h" 
+#include "src/io/input.h" 
+
+#ifdef __cplusplus 
+} 
+#endif 
+
+#endif /* HALOS_H */ 
+
