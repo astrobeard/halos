e5d9b56c219a23b4565b93ccb3c7969e9bb50520
astrobeard <astrobeard21@gmail.com>
2019-12-10 14:26:17 -0500

    plot updates

--- bin/mpl.dm_dspin.py
+++ bin/mpl.dm_dspin.py
@@ -19,22 +19,27 @@ N = sum([len(i) for i in DATA])
 print("Number of hosts: %d" % (N)) 
 
 # take the pieces we're interested in 
-ids = N * [0] 
 dm_over_m = N * [0] 
 dspin_over_spin = N * [0] 
 masses = N * [0] 
+dcvir_over_cvir = N * [0] 
+# dba_over_ba = N * [0] 
+dca_over_ca = N * [0] 
 n = 0
 for i in DATA: 
 	for j in i: 
-		dm_over_m[n] = j[5] / j[4] 
-		masses[n] = j[4] 
+		masses = j[2] 
+		dm_over_m[n] = j[3] / j[2] 
 		dspin_over_spin[n] = j[-1] / j[-2] 
-		ids[n] = j[0]
+		dcvir_over_cvir[n] = j[5] / j[4] 
+		dca_over_dca[n] = j[9] / j[8] 
 		n += 1
-data = [list(i) for i in zip(dm_over_m, dspin_over_spin, masses, ids)]
+data = [list(i) for i in zip(masses, dm_over_m, dspin_over_spin, 
+	dcvir_over_cvir, dca_over_dca)] 
+
 
 def generate_ordered_subsamples(data, n): 
-	indeces = np.argsort([row[0] for row in data]).tolist() 
+	indeces = np.argsort([row[1] for row in data]).tolist() 
 	data = [data[i] for i in indeces] 
 	x = int(len(data) / n) 
 	subs = n * [None] 
@@ -42,21 +47,23 @@ def generate_ordered_subsamples(data, n):
 		subs[i] = data[(x * i) : (x * (i + 1))] 
 	return subs 
 
-def variance(arr): 
-	x = 0.68 
+
+def variance(arr, x = 0.68): 
 	arr = np.sort(arr) 
 	start = int(m.floor((0.5 - x / 2) * len(arr))) 
 	stop = int(m.ceil((0.5 + x / 2) * len(arr))) 
 	return [arr[start], arr[stop]] 
 
-def analyze(sub): 
-	med_dmass = np.median([row[0] for row in sub]) 
-	med_dspin = np.median([row[1] for row in sub]) 
-	yerr = variance([row[1] for row in sub]) 
-	return [med_dmass, med_dspin, yerr[0], yerr[1]] 
 
-def mass_range(data, minlogm, maxlogm, nbins = 100): 
-	filtered = list(filter(lambda x: minlogm <= m.log10(x[2]) <= maxlogm, 
+def analyze(sub, xcol): 
+	med_dx = np.median([row[xcol] for row in sub])
+	med_dspin = np.median([row[2] for row in sub]) 
+	yerr = variance([row[2] for row in sub]) 
+	return [med_dx, med_dspin, yerr[0], yerr[1]] 
+
+
+def mass_range(data, minlogm, maxlogm, xcol, nbins = 100): 
+	filtered = list(filter(lambda x: minlogm <= m.log10(x[0]) <= maxlogm, 
 		data)) 
 	ordered = generate_ordered_subsamples(filtered, nbins) 
 	x = len(ordered) * [0] 
@@ -67,41 +74,105 @@ def mass_range(data, minlogm, maxlogm, nbins = 100):
 		x[i], y[i], low[i], high[i] = analyze(ordered[i]) 
 	return [x, y, low, high] 
 
+
+def setup_axes(): 
+	fig = plt.figure(figsize = (21, 7)) 
+	xlabels = [r"$\Delta M_\text{vir}/M_\text{vir}$", 
+		r"$\Delta c_\text{vir}/c_\text{vir}$", 
+		r"$\Delta(c/a)/(c/a)$"] 
+	axes = 3 * [None] 
+	for i in range(len(axes)): 
+		axes[i] = fig.add_subplot(131 + i, facecolor = "white") 
+		axes[i].set_xlabel(xlabels[i]) 
+	axes[0].set_ylabel(r"$\Delta\lambda/\lambda$") 
+	for i in range(1, len(axes)): 
+		plt.setp(axes[i].get_yticklabels(), visible = False) 
+	return axes 
+
+
+def plot_relation(ax, x, y, low, high, color): 
+	ax.plot(x, y, c = plots.mpltoolkit.named_colors()[color]) 
+	ax.plot(x, low, c = plots.mpltoolkit.named_colors()[color], 
+		linestyle = '--') 
+	ax.plot(x, high, c = plots.mpltoolkit.named_colors()[color], 
+		linestyle = '--') 
+
+
+def legend(ax, colors, labels): 
+	lines = len(colors) * [None] 
+	for i in range(len(colors)): 
+		lines[i] = ax.plot([0, 0.1], [0, 0.1], label = labels[i], 
+			c = plots.mpltoolkit.named_colors()[colors[i]])[0] 
+	leg = ax.legend(loc = plots.mpltoolkit.mpl_loc("lower right"), ncol = 1, 
+		frameon = False, handlelength = 0, fontsize = 20) 
+	for i in range(len(lines)): 
+		lines[i].remove() 
+		leg.get_texts()[i].set_color(colors[i]) 
+
+
 if __name__ == "__main__": 
-	fig = plt.figure(figsize= (7, 7)) 
-	ax = fig.add_subplot(111, facecolor = "white") 
-	ax.set_xlabel(r"$\Delta M / M$") 
-	ax.set_ylabel(r"$\Delta\lambda/\lambda$") 
-	ax.set_xlim([-0.12, 0.22]) 
-	ax.set_ylim([-0.8, 0.8]) 
-	ax.yaxis.set_ticks(np.linspace(-0.8, 0.8, 9)) 
+	plt.clf() 
+	axes = setup_axes() 
 	colors = ["crimson", "lime", "dodgerblue", "black"] 
 	labels = [r"$10^{12\pm0.1}\ h^{-1}\ M_\odot$", 
 			r"$10^{13\pm0.1}\ h^{-1}\ M_\odot$", 
 			r"$10^{14\pm0.1}\ h^{-1}\ M_\odot$", 
-			"All Haloes"]
-	for i in range(len(colors)): 
+			"All Haloes"] 
+	for i in range(4): 
 		if i == 2: 
 			nbins = 50 
 		else: 
 			nbins = 100 
 		x, y, low, high = mass_range(data, [11.9, 12.9, 13.9, 1][i], 
-			[12.1, 13.1, 14.1, 100][i], nbins = nbins)  
-		ax.plot(x, y, c = plots.mpltoolkit.named_colors()[colors[i]], 
-			label = labels[i])  
-		ax.plot(x, low, c = plots.mpltoolkit.named_colors()[colors[i]], 
-			linestyle = '--') 
-		ax.plot(x, high, c = plots.mpltoolkit.named_colors()[colors[i]], 
-			linestyle = '--') 
-	ax.plot(ax.get_xlim(), 2 * [0], 
-		c = plots.mpltoolkit.named_colors()["black"], linestyle = ':') 
-	ax.plot(ax.get_xlim(), 2 * [0.1], 
-		c = plots.mpltoolkit.named_colors()["black"], linestyle = ':') 
-	ax.plot(2 * [0], ax.get_ylim(), 
-		c = plots.mpltoolkit.named_colors()["black"], linestyle = ':') 
-	leg = ax.legend(loc = plots.mpltoolkit.mpl_loc("lower right"), ncol = 1, 
-		frameon = False, handlelength = 0, fontsize = 20) 
-	for i in range(4): 
-		leg.get_texts()[i].set_color(colors[i]) 
+			[12.1, 13.1, 14.1, 100][i], 1, nbins = nbins) 
+		plot_relation(axes[0], x, y, low, high, colors[i]) 
+		x, y, low, high = mass_range(data, [11.9, 12.9, 13.9, 1][i], 
+			[12.1, 13.1, 14.1, 100][i], 3, nbins = nbins) 
+		plot_relation(axes[1], x, y, low, high, colors[i])
+		x, y, low, high = mass_range(data, [11.9, 12.9, 13.9, 1][i], 
+			[12.1, 13.1, 14.1, 100][i], 4, nbins = nbins) 
+		plot_relation(axes[2], x, y, low, high, colors[i]) 
+	plt.subplots_adjust(wspace = 0) 
 	plt.tight_layout() 
 	plt.savefig("dm_dspin.pdf") 
+	plt.clf() 
+
+
+# if __name__ == "__main__": 
+# 	fig = plt.figure(figsize= (7, 7)) 
+# 	ax = fig.add_subplot(111, facecolor = "white") 
+# 	ax.set_xlabel(r"$\Delta M / M$") 
+# 	ax.set_ylabel(r"$\Delta\lambda/\lambda$") 
+# 	# ax.set_xlim([-0.12, 0.22]) 
+# 	# ax.set_ylim([-0.8, 0.8]) 
+# 	# ax.yaxis.set_ticks(np.linspace(-0.8, 0.8, 9)) 
+# 	colors = ["crimson", "lime", "dodgerblue", "black"] 
+# 	labels = [r"$10^{12\pm0.1}\ h^{-1}\ M_\odot$", 
+# 			r"$10^{13\pm0.1}\ h^{-1}\ M_\odot$", 
+# 			r"$10^{14\pm0.1}\ h^{-1}\ M_\odot$", 
+# 			"All Haloes"]
+# 	for i in range(len(colors)): 
+# 		if i == 2: 
+# 			nbins = 50 
+# 		else: 
+# 			nbins = 100 
+# 		x, y, low, high = mass_range(data, [11.9, 12.9, 13.9, 1][i], 
+# 			[12.1, 13.1, 14.1, 100][i], nbins = nbins)  
+# 		ax.plot(x, y, c = plots.mpltoolkit.named_colors()[colors[i]], 
+# 			label = labels[i])  
+# 		ax.plot(x, low, c = plots.mpltoolkit.named_colors()[colors[i]], 
+# 			linestyle = '--') 
+# 		ax.plot(x, high, c = plots.mpltoolkit.named_colors()[colors[i]], 
+# 			linestyle = '--') 
+# 	ax.plot(ax.get_xlim(), 2 * [0], 
+# 		c = plots.mpltoolkit.named_colors()["black"], linestyle = ':') 
+# 	ax.plot(ax.get_xlim(), 2 * [0.1], 
+# 		c = plots.mpltoolkit.named_colors()["black"], linestyle = ':') 
+# 	ax.plot(2 * [0], ax.get_ylim(), 
+# 		c = plots.mpltoolkit.named_colors()["black"], linestyle = ':') 
+# 	leg = ax.legend(loc = plots.mpltoolkit.mpl_loc("lower right"), ncol = 1, 
+# 		frameon = False, handlelength = 0, fontsize = 20) 
+# 	for i in range(4): 
+# 		leg.get_texts()[i].set_color(colors[i]) 
+# 	plt.tight_layout() 
+# 	plt.savefig("dm_dspin.pdf") 
